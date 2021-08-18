68c964a8e2255f76218b3d708131edd7071afe00
Michel <baratella2@gmail.com>
2021-08-18 19:59:10 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Todo from "./components/Todo";
+
+function App() {
+  return (
+    <div>
+      <h1>My To Do's</h1>
+      <Todo text="Learn React" />
+      <Todo text="Master React" />
+      <Todo text="Explore React" />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Backdrop.js
@@ -0,0 +1,5 @@
+function Backdrop(props) {
+  return <div className="backdrop" onClick={props.clicked} />;
+}
+
+export default Backdrop;
